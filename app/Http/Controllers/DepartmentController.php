6b60dcdf9d1829203433a3e6dba42baf65ab8c07
sportsconnect <info@sportsconnect.co>
2020-01-19 13:19:35 +1300

    Bootsrap models and new department creation.

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -5,28 +5,10 @@ namespace App\Http\Controllers;
 use App\Department;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+
 class DepartmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -35,51 +17,23 @@ class DepartmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $name = $request->name;
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Department  $department
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Department $department)
-    {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'name'  =>  'required',
+        ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Department  $department
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Department $department)
-    {
-        //
-    }
+        if($validator->fails()) {
+            $mesg = $validator->messages();
+            return response(json_encode($mesg));
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Department  $department
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Department $department)
-    {
-        //
-    }
+        $d = new Department;
+        $d->fill(['name'=> $name]);
+        $d->save();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Department  $department
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Department $department)
-    {
-        //
+        Session::flash('message', 'Department created!');
+
+        return response()->json('success');
     }
 }
--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,135 +1,161 @@
 /*error messages in validations*/
 function validationError(msg){
-  	html = "<ul class='parsley-errors-list filled' id='parsley-id-7' style='color:red;'><li class='parsley-required'>"+msg+"</li></ul>";
+  html = "<ul class='parsley-errors-list filled' id='parsley-id-7' style='color:red;'><li class='parsley-required'>"+msg+"</li></ul>";
   return html;
 }
 
 /*clear error message*/
 function clearError(){
-  	$(".parsley-errors-list.filled").each(function(){
-    	$(this).empty();
-  	});
+  $(".parsley-errors-list.filled").each(function(){
+    $(this).empty();
+  });
 }
 
 function menuResponsive() {
-  	var x = document.getElementById("myTopnav");
-  	if (x.className === "topnav") {
-    	x.className += " responsive";
-  	} else {
-    	x.className = "topnav";
-  	}
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 function filter(){
-	var str = $("#search").val();
-	var dep = $('#department option:selected').val();
-	// /alert(dep);
-	$.ajaxSetup({
-      	headers: {
-        	'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      	}
+  var str = $("#search").val();
+  var dep = $('#department option:selected').val();
+  // /alert(dep);
+  $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
     });
     $.ajax({
-      	type: "GET",
-      	url: '/filter',
-      	data: {
-		    str: str,
-		    dep: dep,
-      	},
-      	success: function(data){
-      		console.log(data);
-        	$('#filter').html(data);
-      	},
+        type: "GET",
+        url: '/filter',
+        data: {
+        str: str,
+        dep: dep,
+        },
+        success: function(data){
+          console.log(data);
+          $('#filter').html(data);
+        },
     });
 }
 
 $(document).ready(function(){
-	$("#search").on('input', function(){
-    	filter();
-  	});
-  	$("#department").change(function () {
-  		filter();
+  $("#search").on('input', function(){
+      filter();
+    });
+    $("#department").change(function () {
+      filter();
     });
 });
 
 //Staff
 $(document).on("submit", "#new-staff-form", function(event){
-  	event.preventDefault();
-  	$.ajax({
-    	type: "POST",
-    	url: '/staff',
-    	data: new FormData(this),
-    	processData: false,
-    	contentType: false,
-    	success: function(data){
-      		clearError();
-      		if(data=='success'){
-        		location.href = '/';
-      		} else {
-        		var result = JSON.parse(data);
-        		var html = "";
-		        if(result["fname"]){
-		            html = validationError(result["fname"]);
-		            $("#fnameErr").parent().append(html);
-		        }
-		        if(result["lname"]){
-		            html = validationError(result["lname"]);
-		            $("#lnameErr").parent().append(html);
-		        }
-		        if(result["image"]){
-		            html = validationError(result["image"]);
-		            $("#imageErr").parent().append(html);
-		        }
-		        if(result["department"]){
-		            html = validationError(result["department"]);
-		            $("#departmentErr").parent().append(html);
-		        }
-		        if(result["profile"]){
-		            html = validationError(result["profile"]);
-		            $("#profileErr").parent().append(html);
-		        }
-      		}
-    	},
-  	});
+  event.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: '/staff',
+    data: new FormData(this),
+    processData: false,
+    contentType: false,
+    success: function(data){
+      clearError();
+      if(data=='success'){
+        location.href = '/';
+      } else {
+        var result = JSON.parse(data);
+        var html = "";
+        if(result["fname"]){
+            html = validationError(result["fname"]);
+            $("#fnameErr").parent().append(html);
+        }
+        if(result["lname"]){
+            html = validationError(result["lname"]);
+            $("#lnameErr").parent().append(html);
+        }
+        if(result["image"]){
+            html = validationError(result["image"]);
+            $("#imageErr").parent().append(html);
+        }
+        if(result["department"]){
+            html = validationError(result["department"]);
+            $("#departmentErr").parent().append(html);
+        }
+        if(result["profile"]){
+            html = validationError(result["profile"]);
+            $("#profileErr").parent().append(html);
+        }
+      }
+    },
+  });
 });
 
 $(document).on("submit", "#edit-staff-form", function(event){
-  	var form = $('#edit-staff-form');
-  	event.preventDefault();
-  	$.ajax({
-	    type: "POST",
-	    url: form.attr('action'),
-	    data: new FormData(this),
-	    processData: false,
-	    contentType: false,
-	    success: function(data){
-      		clearError();
-      		if(data=='success'){
-        		location.href = '/';
-      		} else {
-        		var result = JSON.parse(data);
-        		var html = "";
-        		if(result["fname"]){
-            		html = validationError(result["fname"]);
-            		$("#fnameErr").parent().append(html);
-        		}
-		        if(result["lname"]){
-		            html = validationError(result["lname"]);
-		            $("#lnameErr").parent().append(html);
-		        }
-		        if(result["image"]){
-		            html = validationError(result["image"]);
-		            $("#imageErr").parent().append(html);
-		        }
-		        if(result["department"]){
-		            html = validationError(result["department"]);
-		            $("#departmentErr").parent().append(html);
-		        }
-		        if(result["profile"]){
-		            html = validationError(result["profile"]);
-		            $("#profileErr").parent().append(html);
-		        }
-      		}
-    	},
-  	});
+  var form = $('#edit-staff-form');
+  event.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: form.attr('action'),
+    data: new FormData(this),
+    processData: false,
+    contentType: false,
+    success: function(data){
+      clearError();
+      if(data=='success'){
+        location.href = '/';
+      } else {
+        var result = JSON.parse(data);
+        var html = "";
+        if(result["fname"]){
+            html = validationError(result["fname"]);
+            $("#fnameErr").parent().append(html);
+        }
+        if(result["lname"]){
+            html = validationError(result["lname"]);
+            $("#lnameErr").parent().append(html);
+        }
+        if(result["image"]){
+            html = validationError(result["image"]);
+            $("#imageErr").parent().append(html);
+        }
+        if(result["department"]){
+            html = validationError(result["department"]);
+            $("#departmentErr").parent().append(html);
+        }
+        if(result["profile"]){
+            html = validationError(result["profile"]);
+            $("#profileErr").parent().append(html);
+        }
+      }
+    },
+  });
+});
+
+
+//Department
+$(document).on("submit", "#new-department-form", function(event){
+  event.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: '/department',
+    data: new FormData(this),
+    processData: false,
+    contentType: false,
+    success: function(data){
+      clearError();
+      if(data=='success'){
+        location.href = '/';
+      } else {
+        var result = JSON.parse(data);
+        var html = "";
+        if(result["name"]){
+            html = validationError(result["name"]);
+            $("#nameErr").parent().append(html);
+        }
+      }
+    },
+  });
 });
